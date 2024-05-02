24fb7c987579f28a4500132b6289fda7ef5dc304
Emiliano Martínez <emilianoruben91@gmail.com>
2024-05-02 16:54:00 -0600

    Add case example

---
+++ case.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "Enter y or n: " ANSWER
+case "$ANSWER" in 
+    [yY]|[yY][eE][sS])
+        echo "You answered yes."
+        ;;
+    [nN]|[oO])
+        echo "You answered no."
+        ;;
+    *)
+        echo "Invalid answer."
+        ;;
+esac

55d84ba9e5b13c0175e6f386b93a5cb2397b2687
Emiliano Martínez <emilianoruben91@gmail.com>
2024-05-03 16:01:18 -0600

    Add debug example

---
+++ debug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Execute in termnial $> bash +x debug.sh
+
+
+FILE_NAME="/not/here"
+ls $FILE_NAME
+echo $FILE_NAME
+
+TEST_VAR="test"
+echo "$TEST_VAR"
+TEST_VAR="nuevo test"
+hostname
